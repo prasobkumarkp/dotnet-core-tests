00f0a2dda18525bb7226614c645791be0ae3a092
Prasob kumar kp <prasobkumarkp@gmail.com>
2020-07-29 00:24:44 +0530

    * .gitignore: MSTest
    * DotNetCoreTests.sln:
    * Program.cs:
    * DotNetCoreTests.csproj:
    * Reservation.cs:
    * ReservationTests.cs:
    * DotNetCoreTests.UnitTests.csproj:

---
+++ DotNetCoreTests.UnitTests/ReservationTests.cs
@@ -0,0 +1,50 @@
+using DotNetCoreTests.Fundamentals;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetCoreTests.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_UserWhoMadeTheRevervation_ReturnsTrue()
+        {
+            // Arrage
+            var reservation = new Reservation();
+            var user = new User();
+            reservation.MadeBy = user;
+
+            // Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUser_ReturnsFalse()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCancelledBy(new User());
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
